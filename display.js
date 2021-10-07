4465419fcbf23c8b172b70b10094379075facf7b
Vimal M <vimalnavee2110@gmail.com>
2021-10-07 13:43:05 +0530

    Upload Section Completed

--- display.js
+++ display.js
@@ -1,17 +1,27 @@
 let obj = JSON.parse(sessionStorage.getItem("key"));
 
 let displayTop = document.getElementsByClassName('display_top')[0];
-let likes = obj.heart + "245";
+
+let likes = 0;
 let likesAdd = false;
+if(obj.heart !==0){
+    likes = obj.heart + (Math.floor(Math.random()*(999-100)+100)).toString();
+}
+
+let view = 1;
+if(obj.view !==0){
+    view = obj.heart + (Math.floor(Math.random()*(999-100)+100)).toString();
+}
+
 let fragment = `
 <img src="${obj.imgurl}" alt="" srcset="">
 <div class="art_des">
-    <h2>Happy Goldy</h2>
+    <h2>${obj.title}</h2>
     <p>  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum repudiandae qui obcaecati quidem eum dolorum vero, amet veritatis maxime error cumque deserunt cum voluptates assumenda consequatur! Corporis earum est consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum consequuntur eveniet similique dolor et a? Modi quisquam, impedit veritatis magnam quaer</p>
     <div>
         <i class=" heart far fa-heart"><span id="likes">${likes}</span></i>
         <i class=" star fas fa-star"><span>${obj.star} star</span></i>
-        <i class=" eye fas fa-eye"><span>${obj.view},625</span></i>
+        <i class=" eye fas fa-eye"><span>${view}</span></i>
     </div>
     <button class="buyBtn"> Buy</button> <br>
     <button class="sponBtn">Sponser</button>
@@ -21,6 +31,7 @@ displayTop.innerHTML = fragment;
 let likeTag = document.getElementsByClassName('heart')[0];
 
 likeTag.addEventListener('click',()=>{
+
     if(likesAdd){
         likesAdd=false;
         likes=parseInt(likes)-1;
@@ -51,5 +62,5 @@ document.getElementById('addBtn').addEventListener('click',()=>{
     if(comment.length>0){
         document.getElementById('comment').innerHTML+=`<p><i class="fas fa-user-circle"></i>Vimal<span>${comment}</span></p>`
     }
+    document.getElementById('input').value = null;
 })
-
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let artContainer = document.getElementById("art-conatiner");
 let star=["3.0","3.5","4.0","4.5"];
+let uploadBtn = document.getElementById('uploadBtn');
 
 document.getElementsByClassName('logo')[0].addEventListener('click',()=>{
     location.href="./index.html";
@@ -9,12 +10,13 @@ document.getElementsByClassName('nav_item')[0].addEventListener('click',()=>{
     location.href="./index.html";
 })
 
-function DataObj(name,imgurl,star,heart,view){
+function DataObj(name,imgurl,star,heart,view,title){
     this.name=name;
     this.imgurl=imgurl;
     this.star=star;
     this.heart=heart;
     this.view=view;
+    this.title=title;
 }
 
 function randomStar(){
@@ -27,36 +29,39 @@ function randomView(){
     return Math.floor(Math.random()*200);
 }
 
+
 let dataArr= [
-    (new DataObj("Virat","./Images/img1.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Mithali","./Images/img2.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Sachin","./Images/img3.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Rahul","./Images/img4.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Dhoni","./Images/img5.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Smirthi","./Images/img6.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Rohit","./Images/img7.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Arjun","./Images/img8.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Priya","./Images/img9.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Raina","./Images/img10.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Rishab","./Images/img11.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Messi","./Images/img12.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Neymar","./Images/img13.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Sunil","./Images/img14.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Geetha","./Images/img15.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Vishal","./Images/img16.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Ashwin","./Images/img17.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Vedha","./Images/img18.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Ajith","./Images/img19.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Lara","./Images/img20.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Warner","./Images/img21.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Smith","./Images/img22.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Root","./Images/img23.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Sara","./Images/img24.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Kavya","./Images/img25.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Surya","./Images/img26.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Amit","./Images/img27.jpg",randomStar(),randomHeart(),randomView())),
-    (new DataObj("Neesham","./Images/img28.jpg",randomStar(),randomHeart(),randomView())),
+    (new DataObj("Virat","./Images/img1.jpg",randomStar(),randomHeart(),randomView(),"Happy Goldy")),
+    (new DataObj("Mithali","./Images/img2.jpg",randomStar(),randomHeart(),randomView(),"Love Birds")),
+    (new DataObj("Sachin","./Images/img3.jpg",randomStar(),randomHeart(),randomView(),"Cartoon Cluster")),
+    (new DataObj("Rahul","./Images/img4.jpg",randomStar(),randomHeart(),randomView(),"Sleeping Princess")),
+    (new DataObj("Dhoni","./Images/img5.jpg",randomStar(),randomHeart(),randomView(),"Real Eye")),
+    (new DataObj("Smirthi","./Images/img6.jpg",randomStar(),randomHeart(),randomView(),"Mafia")),
+    (new DataObj("Rohit","./Images/img7.jpg",randomStar(),randomHeart(),randomView(),"Colorful Roses")),
+    (new DataObj("Arjun","./Images/img8.jpg",randomStar(),randomHeart(),randomView(),"Twin Sparrow")),
+    (new DataObj("Priya","./Images/img9.jpg",randomStar(),randomHeart(),randomView(),"Rabbit Love")),
+    (new DataObj("Raina","./Images/img10.jpg",randomStar(),randomHeart(),randomView(),"Cute Girl")),
+    (new DataObj("Rishab","./Images/img11.jpg",randomStar(),randomHeart(),randomView(),"Melon Day")),
+    (new DataObj("Messi","./Images/img12.jpg",randomStar(),randomHeart(),randomView(),"White Rose")),
+    (new DataObj("Neymar","./Images/img13.jpg",randomStar(),randomHeart(),randomView(),"Face PEncil Art")),
+    (new DataObj("Sunil","./Images/img14.jpg",randomStar(),randomHeart(),randomView(),"Bird Family")),
+    (new DataObj("Geetha","./Images/img15.jpg",randomStar(),randomHeart(),randomView(),"Bathing Sparrow")),
+    (new DataObj("Vishal","./Images/img16.jpg",randomStar(),randomHeart(),randomView(),"Wall Painting")),
+    (new DataObj("Ashwin","./Images/img17.jpg",randomStar(),randomHeart(),randomView(),"Beautiful Roses")),
+    (new DataObj("Vedha","./Images/img18.jpg",randomStar(),randomHeart(),randomView(),"Glorious Flowers")),
+    (new DataObj("Ajith","./Images/img19.jpg",randomStar(),randomHeart(),randomView(),"Spring Bird")),
+    (new DataObj("Lara","./Images/img20.jpg",randomStar(),randomHeart(),randomView(),"Couple Night")),
+    (new DataObj("Warner","./Images/img21.jpg",randomStar(),randomHeart(),randomView(),"Feeling alone")),
+    (new DataObj("Smith","./Images/img22.jpg",randomStar(),randomHeart(),randomView(),"The Eagle")),
+    (new DataObj("Root","./Images/img23.jpg",randomStar(),randomHeart(),randomView(),"Black Kitten")),
+    (new DataObj("Sara","./Images/img24.jpg",randomStar(),randomHeart(),randomView(),"Oil Painting")),
+    (new DataObj("Kavya","./Images/img25.jpg",randomStar(),randomHeart(),randomView(),"Blue Tree")),
+    (new DataObj("Surya","./Images/img26.jpg",randomStar(),randomHeart(),randomView(),"Cool Girl")),
+    (new DataObj("Amit","./Images/img27.jpg",randomStar(),randomHeart(),randomView(),"Women Life")),
+    (new DataObj("Neesham","./Images/img28.jpg",randomStar(),randomHeart(),randomView(),"My Cute Kitten")),
 ]
+
+function dispaly(){
 
 for(let i =0;i<dataArr.length;i++){
 let fragment = `
@@ -71,13 +76,40 @@ let fragment = `
           </div>
        </div>
   </div>
-
 `
 artContainer.innerHTML+=fragment;
 }
+}
+
+dispaly();
 
 function displayArt(art){
    let key = art.id;
    sessionStorage.setItem("key",JSON.stringify(dataArr[key-1]));
    window.location.href="./display.html";
+}
+
+uploadBtn.onclick=()=>{
+    document.getElementById('art-conatiner').style.filter = "blur(30px)";
+    document.getElementById('uploadForm').style.display = "block";
+}
+
+document.getElementById('closeForm').onclick=()=>{
+    document.getElementById('art-conatiner').style.filter = "blur(0)";
+    document.getElementById('uploadForm').style.display = "none";
+}
+
+document.getElementById('formBtn').onclick=()=>{
+    let newTitle = document.getElementById('newTitle').value;
+    let newImg =  document.getElementById('newImg');
+    const objectURL = URL.createObjectURL(newImg.files[0]);
+
+
+    console.log(objectURL);
+
+    dataArr.push((new DataObj("Vimal",objectURL,0,0,0,newTitle)));
+    dispaly();
+    document.getElementById('art-conatiner').style.filter = "blur(0)";
+    document.getElementById('uploadForm').style.display = "none";
+
 }
